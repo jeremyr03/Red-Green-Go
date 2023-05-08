116a3ed89c31a2a4fc5bb98d52370984c8a38d1d
t11915jr <jeremy.roe@student.manchester.ac.uk>
2023-05-08 15:35:10 +0100

    Added tests

--- roman-numerals-converter/test/uk/ac/manchester/cs/comp33711/romannumerals/test/RomanNumeralTest.java
+++ roman-numerals-converter/test/uk/ac/manchester/cs/comp33711/romannumerals/test/RomanNumeralTest.java
@@ -10,8 +10,50 @@ public class RomanNumeralTest {
 	
 	@Test
 	public void shouldConvertArabicOne(){
-		RomanNumeral romanNumeral = new RomanNumeral(???);
-		assertEquals("???", romanNumeral.toString());
+		RomanNumeral romanNumeral = new RomanNumeral(1);
+		assertEquals("I", romanNumeral.toString());
 	}
+
+    @Test
+    public void shouldConvertArabicTwo(){
+        RomanNumeral romanNumeral = new RomanNumeral(2);
+        assertEquals("II", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouoldConvertArabicThree(){
+        RomanNumeral romanNumeral = new RomanNumeral(3);
+        assertEquals("III", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouoldConvertArabicFour(){
+        RomanNumeral romanNumeral = new RomanNumeral(4);
+        assertEquals("IV", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouldConvertArabicFive(){
+        RomanNumeral romanNumeral = new RomanNumeral(5);
+        assertEquals("V", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouldConvertArabicSix(){
+        RomanNumeral romanNumeral = new RomanNumeral(6);
+        assertEquals("VI", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouldConvertArabicTen(){
+        RomanNumeral romanNumeral = new RomanNumeral(10);
+        assertEquals("X", romanNumeral.toString());
+    }
+
+    @Test
+    public void shouldConvertArabicEleven(){
+        RomanNumeral romanNumeral = new RomanNumeral(11);
+        assertEquals("XI", romanNumeral.toString());
+    }
 
 }
