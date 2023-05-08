dd1dea5cdd5b7f2e31bb1928b70893d863a71572
t11915jr <jeremy.roe@student.manchester.ac.uk>
2023-05-08 15:35:52 +0100

    Implemented roman numeral conversion

--- roman-numerals-converter/src/uk/ac/manchester/cs/comp33711/romannumerals/RomanNumeral.java
+++ roman-numerals-converter/src/uk/ac/manchester/cs/comp33711/romannumerals/RomanNumeral.java
@@ -2,12 +2,30 @@ package uk.ac.manchester.cs.comp33711.romannumerals;
 
 public class RomanNumeral {
 
-	public RomanNumeral(int i) {
+    private int denary;
 
-	}
-	
-	public String toString(){
-		return null;
-	}
-	
+    public RomanNumeral(int i) {
+        denary = i;
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        if(denary/10 >= 1){
+            denary -= 10;
+            result.append("X");
+        }
+        if (denary / 5 >= 1) {
+            denary -= 5;
+            result.append("V");
+        }
+        if (denary == 4) {
+            result.append("IV");
+        } else {
+            result.append("I".repeat(Math.max(0, denary)));
+        }
+
+        return result.toString();
+    }
+
 }
